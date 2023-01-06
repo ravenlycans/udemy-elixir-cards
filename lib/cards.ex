85b4c6563771434d1756035f37e671f68a6e1113
RavenLycans <raven@lycans.dk>
2023-01-06 20:11:43 +0100

    Updated project to lesson 30.

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a list of playing cards
+  """
   def create_deck do
     cards = [
       "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"
@@ -13,6 +20,16 @@ defmodule Cards do
 
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+      iex> deck = Cards.create_deck()
+      iex> {hand,deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -21,10 +38,30 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines wheather a deck contains a given card.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, hand) do
     Enum.member?(deck, hand)
   end
 
+  @doc """
+    Creates a deck, shuffles it and deals `hand_size` cards, plus the remaining cards in the deck.
+
+  ## Examples
+
+      iex> { hand, deck } = Cards.create_hand(5)
+      iex> Enum.count(hand)
+      5
+      iex> Cards.contains?(deck, hand)
+      false
+  """
   def create_hand(hand_size) do
 
     Cards.create_deck()
